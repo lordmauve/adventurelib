6782c326ca1f55316ec28c616e5f3ac908dae410
Daniel Pope <mauve@mauveweb.co.uk>
2016-09-18 22:28:06 +0100

    Add Python packaging config

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='adventurelib',
+    description='Easy text adventures',
+    version='1.0a1',
+    author='Daniel Pope',
+    author_email='mauve@mauveweb.co.uk',
+    url='https://adventurelib.readthedocs.io/',
+    py_modules=['adventurelib']
+)
