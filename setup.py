c1636188c4da49218d6d3e3b901a4519ceec0cde
Daniel Pope <mauve@mauveweb.co.uk>
2016-10-01 01:48:37 +0100

    Bump version to 1.0

--- setup.py
+++ setup.py
@@ -4,9 +4,17 @@ from setuptools import setup
 setup(
     name='adventurelib',
     description='Easy text adventures',
-    version='1.0a1',
+    version='1.0',
     author='Daniel Pope',
     author_email='mauve@mauveweb.co.uk',
     url='https://adventurelib.readthedocs.io/',
-    py_modules=['adventurelib']
+    py_modules=['adventurelib'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Education',
+        'Topic :: Games/Entertainment',
+    ]
 )
